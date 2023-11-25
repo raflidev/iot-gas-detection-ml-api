8fb7c8805d75b8253c25f0e5383818fb490ee320
Rafli Ramadhan <rafliramdhn@gmail.com>
2023-11-25 21:41:33 +0700

    add cors

--- api.py
+++ api.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, send_file
 from werkzeug.utils import secure_filename
+from flask_cors import CORS, cross_origin
 import script as model
-app = Flask(__name__)
-
+app = Flask(__name__) 
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+@cross_origin
 @app.route("/")
 def home():
     return "<p>Hello, World!</p>"
@@ -12,8 +15,12 @@ def cek():
   if request.method == 'POST':
     # f = request.files['file']
     # f.save(secure_filename(f.filename))
-    mq7 = request.form['mq7']
-    mq135 = request.form['mq135']
+    # mq7 = request.form['mq7']
+    # mq135 = request.form['mq135']
+    # json
+    data = request.get_json()
+    mq7 = data['mq7']
+    mq135 = data['mq135']
     return model.predict(mq7, mq135)
 
 if __name__ == "__name__":
